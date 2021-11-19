8ffb0ed462620ad7de669a08fecdf6f2616d6a7b
Maxime Préard <maxime.preard@hotmail.fr>
2021-11-19 13:58:32 +0100

    clean

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
 // Imports
 var readFilter = require ("./filters/read.js");
-var qoa = require('qoa');
-
-// Constantes
-const {log} = console;
-const input = {
-    type: 'input',
-    query: 'Write command : ',
-    handle: 'command'
-};
-
+var configFilters = require ("./config-filters.json");
 
 // Test import READ.JS
-// let test = readFilter('./test');
+console.log(readFilter('./test'));
 
-// console.log(test)
- 
-
-
-
-console.clear();
-console.log("################")
-console.log("##   PiperJS  ##")
-console.log("################")
-console.log("")
-console.log("List commands.")
-console.log("ppft new -> Initiate new project")
-console.log("ppft add_filter -> Create a new filter")
-console.log("ppftdel_filter -> Delete an existing filter")
-console.log("ppftadd_step -> Add a new step to the configuration file")
-console.log("ppftdel_step -> Delete a step from configuration file")
-console.log("")
-// using the `prompt` async method
-qoa.prompt([input]).then(log);
-//=> { username: 'klaussinani' }
-
-// using the `input` async method
-qoa.input(input).then(log);
-//=> { username: 'klaussinani' }
--- filters/read.js
+++ filters/read.js
@@ -2,20 +2,8 @@ const fs = require("fs");
 
 module.exports = (path) => {
     fs.readFile(path + ".txt", (error, data) => {
-        if(error) {
-            throw error;
-        }
+        if (error) throw error;
         // console.log(data.toString())
         return data.toString();
     });
 }
-
-// TEST //
-// let path = './test'
-// fs.readFile(path + ".txt", (error, data) => {
-//     if(error) { 
-//         throw error;
-//     }
-//     console.log(data.toString())
-//     // return data.toString();
-// });
