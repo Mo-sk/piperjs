8b73b7c398f7af04a25d7e9817f2e8eaea2d25a8
Maxime Préard <maxime.preard@hotmail.fr>
2021-11-19 12:31:00 +0100

    add read filter

---
+++ app.js
@@ -0,0 +1,5 @@
+var readFilter = require ("./filters/read.js");
+
+let test = readFilter('./test');
+
+console.log(test)
--- filters/read.js
+++ filters/read.js
@@ -1,3 +1,21 @@
-module.exports = (input) => {
-    
+const fs = require("fs");
+
+module.exports = (path) => {
+    fs.readFile(path + ".txt", (error, data) => {
+        if(error) {
+            throw error;
+        }
+        // console.log(data.toString())
+        return data.toString();
+    });
 }
+
+// TEST //
+// let path = './test'
+// fs.readFile(path + ".txt", (error, data) => {
+//     if(error) { 
+//         throw error;
+//     }
+//     console.log(data.toString())
+//     // return data.toString();
+// });
